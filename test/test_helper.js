21b81cf28dcc6c7cb97641bf63439bfc2e69c087
Brett <brett@applogix.com>
2016-08-09 19:38:10 -0700

    initial project setup

--- test/test_helper.js
+++ test/test_helper.js
@@ -8,3 +8,14 @@ const win = doc.defaultView;
 //Ensures will be discovered by React when it accesses document or window
 global.document = doc;
 global.window = win;
+
+/*ALSO: Take all properties from jsdom window object (eg. navigator) & hoist
+* onto Node.js global object:
+* - ensures properties provided by window can be used without the 'window.' prefix
+*   as would happen in a browser environment.
+* - this is because some of the code in React relies on this */ 
+Object.keys(window).forEach((key) => {
+  if (!(key in global)) {
+    global[key] = window[key];
+  }
+});
